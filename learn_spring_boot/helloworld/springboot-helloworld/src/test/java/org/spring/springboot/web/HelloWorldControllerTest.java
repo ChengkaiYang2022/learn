6ff01d26f8afa27f54a91a674bdc4b8d73b05e2f
kk <18810276930@163.com>
2018-06-29 14:46:39 +0800

    init

---
+++ learn_spring_boot/helloworld/springboot-helloworld/src/test/java/org/spring/springboot/web/HelloWorldControllerTest.java
@@ -0,0 +1,14 @@
+package org.spring.springboot.web;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class HelloWorldControllerTest {
+
+	@Test
+	public void testSayHello() {
+		assertEquals("Hello,World!",new HelloWorldController().sayHello());
+	}
+
+}

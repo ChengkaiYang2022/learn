438d94b59802d950e322419b5171e4fd8db4024d
forrestlmj <18810276930@163.com>
2018-08-12 19:27:41 +0800

    retirement

---
+++ Retirement2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Retirement2 {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.println("How much money will you contribute every year? ");
+        double payment = in.nextDouble();
+
+        System.out.println("Interest rate in %:");
+        double interestRate = in.nextDouble();
+
+        double balance = 0;
+        int years = 0;
+
+        String input;
+        do{
+            balance += payment;
+            double interest = balance*interestRate/100;
+            balance += interest;
+            years++;
+            System.out.printf("After %d years, your balance is %f .\n",years, balance);
+
+            System.out.println("Ready to retire? (Y/N) ");
+            input = in.next();
+        }while(!input.equalsIgnoreCase("y"));
+    }
+}
--- scanner.java
+++ scanner.java
@@ -1,8 +1,24 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class scanner {
+    public static void readfile()  {
+        try{
+            //Scanner in = new Scanner(Paths.get("README.md"));
+            //System.out.println(in);
+            PrintWriter out = new PrintWriter("README.md");
+            System.out.println(out);
+        }catch (IOException e){
+            System.out.println(e);
+        }
+
+    }
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
+        /**
         System.out.println("What is your name?");
         String name = in.nextLine();
 
@@ -10,5 +26,9 @@ public class scanner {
         int age = in.nextInt();
 
         System.out.println("Hello, " + name + ". Next year,you'll be " + (age + 1));
+        */
+        readfile();
     }
+
+
 }

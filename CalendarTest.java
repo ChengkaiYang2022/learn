dd749ba60fa247e0fc241d4de5033f9e8cf01b8c
forrestlmj <18810276930@163.com>
2018-08-30 22:39:23 +0800

    CalendarTest

---
+++ CalendarTest.java
@@ -0,0 +1,39 @@
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+public class CalendarTest {
+    public static void main(String[] args){
+        LocalDate date = LocalDate.now();
+        int month = date.getMonthValue();
+        int today = date.getDayOfMonth();
+
+        date = date.minusDays(today - 1);
+        System.out.println(month);
+        System.out.println(date);
+
+        DayOfWeek weekday = date.getDayOfWeek();
+        int value = weekday.getValue();
+
+        System.out.println("Mon Tue Wed Thu Fri Sat Sun");
+        for(int i = 1;i < value; i++){
+            System.out.println("  ");
+        }
+        while(date.getMonthValue() == month){
+            System.out.printf("%3d",date.getDayOfMonth());
+            if(date.getDayOfMonth() == today){
+                System.out.print("*");
+            }else{
+                System.out.print(" ");
+            }
+            date = date.plusDays(1);
+            if(date.getDayOfWeek().getValue() == 1){
+                System.out.println();
+            }
+
+        }
+        if(date.getDayOfWeek().getValue() != 1){
+            System.out.println();
+        }
+
+    }
+}

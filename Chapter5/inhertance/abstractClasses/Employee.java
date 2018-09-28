17d1bf4259f09d98db021a7c609d5b8b505d2dfa
forrestlmj <18810276930@163.com>
2018-09-28 22:51:22 +0800

    persontest

--- Chapter5/inhertance/abstractClasses/Employee.java
+++ Chapter5/inhertance/abstractClasses/Employee.java
@@ -1,4 +1,32 @@
 package Chapter5.inhertance.abstractClasses;
 
-public class Employee {
+import java.time.LocalDate;
+
+public class Employee extends Person {
+    private double salary;
+    private LocalDate hireDay;
+
+    public Employee(String name,double salary,int year,int month,int day){
+        super(name);
+        this.salary = salary;
+        this.hireDay = LocalDate.of(year,month,day);
+    }
+
+    @Override
+    public String getDescription() {
+        return String.format("an employee with salary of $%.2f",salary);
+    }
+
+    public double getSalary(){
+        return salary;
+    }
+
+    public LocalDate getHireDay(){
+        return hireDay;
+    }
+
+    public void raiseSalary(double byPercent){
+        double raise = salary * byPercent / 100;
+        salary += raise;
+    }
 }
--- Chapter5/inhertance/abstractClasses/PersonTest.java
+++ Chapter5/inhertance/abstractClasses/PersonTest.java
@@ -1,4 +1,13 @@
 package Chapter5.inhertance.abstractClasses;
 
 public class PersonTest {
+    public static void main(){
+        Person[] people = new Person[2];
+        people[0] = new Employee("Harry Hacker",5000,1989,10,1);
+        people[1] = new Student("Maria Morris","computer sciencs");
+
+        for (Person p:people){
+            System.out.println(p.getName() + ", " + p.getDescription());
+        }
+    }
 }

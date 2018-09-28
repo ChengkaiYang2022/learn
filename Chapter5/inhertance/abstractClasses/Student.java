c0da973cd774fa9841e50e49b1d669589ecc581d
forrestlmj <18810276930@163.com>
2018-09-28 21:51:50 +0800

    Student类 重写getDescription函数,同时由于集成了person类,构造函数也要有

--- Chapter5/inhertance/abstractClasses/Student.java
+++ Chapter5/inhertance/abstractClasses/Student.java
@@ -1,4 +1,14 @@
 package Chapter5.inhertance.abstractClasses;
 
-public class Student {
+public class Student extends Person{
+    private String major;
+
+    public Student(String name,String major){
+        super(name);
+        this.major = major;
+    }
+
+    public String getDescription(){
+       return "a student major in " + major;
+    }
 }

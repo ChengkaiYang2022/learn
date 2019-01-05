db9e8de2c7d64b9a9124632ea8eab6238e8c5ebc
forrestlmj <18810276930@163.com>
2019-01-05 15:13:32 +0800

    泛型与反射

--- Chapter8/pair1/genericReflection/GenericReflectionTest.java
+++ Chapter8/pair1/genericReflection/GenericReflectionTest.java
@@ -1,5 +1,7 @@
 package Chapter8.pair1.genericReflection;
 
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
 import java.lang.reflect.*;
 import java.util.*;
 
@@ -25,7 +27,8 @@ public class GenericReflectionTest
             // print generic info for class and public methods
             Class<?> cl = Class.forName(name);
             printClass(cl);
-
+            for (Method m : cl.getDeclaredMethods())
+                printMethod(m);
         }
         catch (ClassNotFoundException e){
             e.printStackTrace();
@@ -34,6 +37,26 @@ public class GenericReflectionTest
     public static void printClass(Class<?> cl){
         System.out.println(cl);
         printTypes(cl.getTypeParameters(),"<",",",">",true);
+        Type sc = cl.getGenericSuperclass();
+        if(sc != null){
+            System.out.print(" extends ");
+            printType(sc, false);
+        }
+        printTypes(cl.getGenericInterfaces(), " implements ",",",",",false);
+        System.out.println();
+    }
+    public static void printMethod(Method m){
+        String name = m.getName();
+        System.out.print(Modifier.toString(m.getModifiers()));
+        System.out.print(" ");
+        printTypes(m.getTypeParameters(),"<",",",">",true);
+
+        printType(m.getGenericReturnType(),false);
+        System.out.print(" ");
+        System.out.print(name);
+        System.out.print("(");
+        printTypes(m.getGenericParameterTypes(),"",",","",false);
+        System.out.println(")");
     }
     public static void printTypes(Type[] types, String pre, String sep, String suf, boolean isDefinition){
         if(pre.equals(" extends ") && Arrays.equals(types,new Type[]{ Object.class})) return;
@@ -42,6 +65,7 @@ public class GenericReflectionTest
             if(i > 0) System.out.print(sep);
             printType(types[i],isDefinition);
         }
+        if(types.length > 0) System.out.print(suf);
     }
     public static void printType(Type type,boolean isDefinition){
         if(type instanceof Class){
@@ -52,11 +76,29 @@ public class GenericReflectionTest
             System.out.print(t.getName());
             if(isDefinition){
                 printTypes(t.getBounds(), " extends ", " & ", "", false);
-
             }
+        }else if(type instanceof WildcardType){
+            WildcardType t = (WildcardType) type;
+            System.out.print("?");
+            printTypes(t.getUpperBounds()," extends "," & ","",false);
+            printTypes(t.getLowerBounds()," super "," & ","",false);
+        }else if(type instanceof ParameterizedType)
+        {
+            ParameterizedType t = (ParameterizedType) type;
+            Type owner = t.getOwnerType();
+            if(owner != null){
+                printType(owner,false);
+                System.out.print(".");
+            }
+            printType(t.getRawType(), false);
+            printTypes(t.getActualTypeArguments(),"<",",",">",false);
+        }
+        else if(type instanceof GenericArrayType){
+            GenericArrayType t = (GenericArrayType) type;
+            System.out.print("");
+            printType(t.getGenericComponentType(),isDefinition);
+            System.out.print("[]");
         }
     }
-
-
 }
 

be7cfd1271b6b508881df39ff0ad885b158498fb
forrestlmj <18810276930@163.com>
2019-01-10 20:31:00 +0800

    TreeSet

--- Chapter9/treeSet/TreeSetTest.java
+++ Chapter9/treeSet/TreeSetTest.java
@@ -1,5 +1,7 @@
 package Chapter9.treeSet;
 
+import java.util.Comparator;
+import java.util.NavigableSet;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
@@ -14,8 +16,18 @@ public class TreeSetTest
 {
     public static void main(String[] args){
         SortedSet<Item> parts = new TreeSet<>();
-//        parts.add(a);
-        parts.add(new Item("Toaster",1234));
 
+        parts.add(new Item("Toaster",1234));
+        parts.add(new Item("Widget",4562));
+        parts.add(new Item("Modem",9912));
+
+        System.out.println(parts);
+
+        NavigableSet<Item> sortByDescription = new TreeSet<>(
+                Comparator.comparing(Item::getDescription)
+        );
+
+        sortByDescription.addAll(parts);
+        System.out.println(sortByDescription);
     }
 }

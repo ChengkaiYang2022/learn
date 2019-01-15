00b192468042732ccb4db67b2d0964cc0d9f78bf
forrestlmj <18810276930@163.com>
2019-01-15 22:22:53 +0800

    subList 视图与shuffle洗牌

---
+++ Chapter9/shuffle/ShuffleTest.java
@@ -0,0 +1,21 @@
+package Chapter9.shuffle;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ShuffleTest {
+    public static void main(String[] args)
+    {
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 1;i<=49;i++){
+            numbers.add(i);
+        }
+        Collections.shuffle(numbers);
+//        System.out.println(numbers);
+        List<Integer> winningCombination = numbers.subList(0, 6);
+        Collections.sort(winningCombination);
+
+        System.out.println(winningCombination);
+    }
+}

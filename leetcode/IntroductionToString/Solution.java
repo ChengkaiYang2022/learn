7b7bbd28096cff28a9051dc911c36ace4f441c73
forrestlmj <18810276930@163.com>
2019-03-09 23:18:12 +0800

    addBinary

---
+++ leetcode/IntroductionToString/Solution.java
@@ -0,0 +1,31 @@
+package leetcode.IntroductionToString;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class Solution{
+    public String addBinary(String a,String b){
+        String r="";
+
+        System.out.println();
+        return r;
+    }
+}
+class myTest{
+    private Solution solution = new Solution();
+
+    @Test
+    public void testCase1(){
+        String a = "11";
+        solution.addBinary(a,a);
+//        String b = "1";
+//        String r = "100";
+//        Assertions.assertEquals(solution.addBinary(a,b),r);
+//        a = "1010"; b = "1011"; r = "10101";
+//        Assertions.assertEquals(solution.addBinary(a,b),r);
+
+    }
+}
